558b241747c0771dbf22c3a35c9c946216bf05d0
Vitalii Navrotskyi <vitalii.navrotskyi@midnight.works>
2025-05-01 12:42:11 +0300

    Initial Commit

---
+++ ListDetailTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ListDetailTest
+//
+//  Created by Vitalii Navrotskyi on 01.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

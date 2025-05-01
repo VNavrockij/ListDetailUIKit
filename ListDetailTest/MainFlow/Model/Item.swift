ebd374e636d445b725ce400e8fca781af54734df
Vitalii Navrotskyi <vitalii.navrotskyi@midnight.works>
2025-05-01 13:08:32 +0300

    start new project

---
+++ ListDetailTest/MainFlow/Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  ListDetailTest
+//
+//  Created by Vitalii Navrotskyi on 01.05.2025.
+//
+
+import Foundation
+
+struct Item: Decodable {
+    let id: Int
+    let title: String
+    let description: String
+}
